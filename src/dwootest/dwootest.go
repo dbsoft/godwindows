3e2c45919ca7abc2edd1dd2d728eb76f9a00a8ae
Brian Smith <brian@dbsoft.org>
2013-03-05 14:43:29 -0600

    Minor changes to make it as simple as possible.

--- src/dwootest/dwootest.go
+++ src/dwootest/dwootest.go
@@ -5,11 +5,6 @@ import (
 )
 
 // Global variables
-const (
-   FALSE int = iota
-   TRUE
-)
-
 var APP_NAME = "DW Object Oriented Example"
 
 /* Handle exiting the application */
@@ -18,23 +13,23 @@ func exit_handler(win dw.HWND, message string) int {
         /* Exit the application cleanly */
         dw.Main_quit();
     }
-    return TRUE;
+    return dw.TRUE;
 }
 
-var exit_handler_func = exit_handler;
-
 func main() {
+    var message = "Are you sure you want to exit?";
+    
     /* Initialize Dynamic Windows */
-    dw.Init(TRUE);
+    dw.Init(dw.TRUE);
 
     /* Create our window */
     window := dw.Window_new(dw.DESKTOP, APP_NAME, dw.FCF_SYSMENU | dw.FCF_TITLEBAR | dw.FCF_TASKLIST | dw.FCF_DLGBORDER | dw.FCF_SIZEBORDER | dw.FCF_MINMAX);
 
     label := dw.Text_new("Hello, World", 0);
-    dw.Box_pack_start(window, label, 0, 0, TRUE, TRUE, 0);
+    dw.Box_pack_start(window, label, 0, 0, dw.TRUE, dw.TRUE, 0);
     
     /* Connect the signal handlers */
-    window.Delete(func(window dw.HWND, data dw.POINTER) int { return exit_handler(window, "Are you sure you want to exit?"); });
+    window.Delete(func(window dw.HWND, data dw.POINTER) int { return exit_handler(window, message); });
 
     dw.Window_set_size(window, 640, 550);
     dw.Window_show(window);
